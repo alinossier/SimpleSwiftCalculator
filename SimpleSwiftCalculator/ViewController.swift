8b8ce46952325e24c38eecb81d2d91cdd369ad68
anto0522 <antoninlinossier1@macbook-de-antonin-linossier-2.local>
2017-05-28 21:48:01 -0400

    Initial Commit

---
+++ SimpleSwiftCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleSwiftCalculator
+//
+//  Created by Antonin Linossier on 5/28/17.
+//  Copyright © 2017 Antonin Linossier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
